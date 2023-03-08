df8f6ea265074d9eb3f266991efdfdc9fc1b0efb
yelleen <yumizu@naver.com>
2023-03-08 22:47:21 +0900

    Turn.java is modified Singleton

--- Resources/src/process/Turn.java
+++ Resources/src/process/Turn.java
@@ -1,20 +1,67 @@
 package process;
 
 import java.util.ArrayList;
-import cards.Card;
+import cards.*;
 
 public class Turn {
-    public static int turn = 1;
-    public static Pack main = new Pack();
-    public static Pack pale = new Pack();
-    public static Card pre;
-    public static Card now; // -> TODO at the first turn this is assigned by random card
-    private static int direction = 1;
-    private ArrayList<Player> players = new ArrayList<Player>();
-    public static void Process() {
-
+    private static Turn instance;
+    private int turn;
+    private int order;
+    private final Pack main;
+    private final Pack pale;
+    private Card pre;
+    private Card now;
+    private int direction;
+    private ArrayList<Player> players;
+    public Turn() {
+        turn = 1;
+        order = 1;
+        main = new Pack();
+        pale = new Pack();
+        pre = new cards.Number(1, "pre");
+        now = new cards.Number(1, "now");
+        direction = 1;
+        players = new ArrayList<Player>();
     }
-    public static void SetDirection() {
+    public static Turn GetInstance() {
+        if(instance == null) {
+            instance = new Turn();
+        }
+        return instance;
+    }
+    public int GetTurn() {
+        return turn;
+    }
+    public int GetOrder() {
+        return order;
+    }
+    public Pack GetMain() {
+        return main;
+    }
+    public Pack GetPale() {
+        return pale;
+    }
+    public Card GetPre() {
+        return pre;
+    }
+    public Card GetNow() {
+        return now;
+    }
+    public void SetTurn() {
+        turn++;
+    }
+    //param is Player.order
+    public void SetOrder(int o) {
+        order = o + 1;
+        order = order > players.size() ? 1 : order;
+    }
+    public void SetPre(Card p) {
+        pre = p;
+    }
+    public void SetNow(Card n) {
+        now = n;
+    }
+    public void SetDirection() {
         direction = direction > 0 ? -1 : 1;
     }
 }
