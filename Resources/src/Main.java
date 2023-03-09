1b0f6cff59eb2f0f3b56bd01f0657e7baf5c6e17
yelleen <yumizu@naver.com>
2023-03-09 23:58:21 +0900

    players can join the game now

--- Resources/src/Main.java
+++ Resources/src/Main.java
@@ -1,5 +1,21 @@
+import process.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("GameStart");
+        Player p1 = new Player("First");
+        Player p2 = new Player("Second");
+        Player p3 = new Player("Third");
+        Player p4 = new Player("Fourth");
+        Player p5 = new Player("Fifth");
+        Player p6 = new Player("Sixth");
+        p1.JoinGame();
+        p2.JoinGame();
+        p3.JoinGame();
+        p4.JoinGame();
+        p5.JoinGame();
+        p6.JoinGame();
+        Turn.GetInstance().SetOrderAllPlayers();
+        System.out.println("GameEnd");
     }
 }
