c2f97e1d8601fd2437ff6d33349c97dccae05497
yelleen <yumizu@naver.com>
2023-03-13 23:25:53 +0900

    Now Server class is only single class and it's for players to create or join room

---
+++ Resources/src/process/Server.java
@@ -0,0 +1,40 @@
+package process;
+
+import java.util.ArrayList;
+
+public class Server {
+    private static Server instance;
+    private ArrayList<Room> rooms = new ArrayList<Room>();
+
+    private Server() {}
+
+    public static Server GetInstance() {
+        if(instance == null) {
+            instance = new Server();
+        }
+        return instance;
+    }
+    public Room CreateNewRoom(String pw) {
+        Room room = new Room();
+        room.SetPassword(pw);
+        rooms.add(room);
+        room.SetIndex(rooms.size());
+        return room;
+    }
+    public Room MatchingRoom(String pw) {
+        Room room = new Room();
+        boolean found = false;
+        for (Room r : rooms) {
+            if (r.FindByPassword(pw) != null) {
+                room = r;
+                found = true;
+            }
+        }
+        if(!found) {
+            System.out.println("Room is not found Create Room");
+            rooms.add(room);
+            room.SetIndex(rooms.size());
+        }
+        return room;
+    }
+}
